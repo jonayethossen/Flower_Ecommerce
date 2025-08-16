f427c3125518b263befb72d3f7c383e1aae24b25
jonayethossen <jonayethossen8921@gmail.com>
2025-08-17 01:06:28 +0600

    start the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return (
-    <div className="bg-red-500">
-      <h1>Jonayet hossen moman</h1>
-      <h2>JH Moman </h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem laborum
-        labore placeat nesciunt dolorum voluptatibus officia explicabo iste
-        quaerat, dolores id ipsum natus aliquam quisquam eaque accusantium modi
-        a. Cumque natus officia, qui labore cum nobis. Maiores non, eveniet,
-        amet, deleniti placeat fuga vel magnam iure architecto distinctio
-        doloremque facilis?
-      </p>
-    </div>
-  );
+  return <div>Lorem ipsum dolor sit amet.</div>;
 };
 
 export default App;
