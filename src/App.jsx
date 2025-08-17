e0f5917a0f15e0da104f1f93f68764572a52589a
jonayethossen <jonayethossen8921@gmail.com>
2025-08-17 23:34:44 +0600

    Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import HeroSection from "./components/Hero/HeroSection";
 
 const App = () => {
   return (
     <div className="overflow-x-hidden">
       <Navbar />
+      <HeroSection />
     </div>
   );
 };
