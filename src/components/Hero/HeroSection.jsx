352fd1a312784b49c3485f023c0eb90a3d5a1854
jonayethossen <jonayethossen8921@gmail.com>
2025-08-18 22:31:54 +0600

    Nav

--- src/components/Hero/HeroSection.jsx
+++ src/components/Hero/HeroSection.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const HeroSection = () => {
-  return <div></div>;
+  return (
+    <div>
+      {" "}
+      jonayet hossen <span>Moman</span>
+    </div>
+  );
 };
 
 export default HeroSection;
