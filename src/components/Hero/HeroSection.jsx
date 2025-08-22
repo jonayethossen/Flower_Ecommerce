c9b5f84623a921440d3d04c3ffa6d28d839a3e11
jonayethossen <jonayethossen8921@gmail.com>
2025-08-22 12:24:45 +0600

    Nothing to do

--- src/components/Hero/HeroSection.jsx
+++ src/components/Hero/HeroSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const HeroSection = () => {
   return (
     <section>
-      <div>jonayet hossen Moman</div>
+      <div className="w-8 h-8 bg-green-500 rounded-full flex items-center justify-center"></div>
     </section>
   );
 };
